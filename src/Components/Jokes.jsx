3b5805dd121edad65090cca889c1e00279d063d4
Nidhi <nidhiparihar1309@gmail.com>
2025-09-29 12:38:17 -0700

    commit

--- src/Components/Jokes.jsx
+++ src/Components/Jokes.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import Button from "./Button"
-import '../style/joke.css'
+import Button from "./Button";
+import '../style/joke.css';
 
 const Jokes = () => {
-  const [joke,setJoke] = useState("");
+  const [joke, setJoke] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const fetchApi = () =>{
-            fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
-
-          .then((res) => res.json())
-          .then((data) => setJoke(data.joke));
+  const fetchApi = () => {
+    setLoading(true);
+    fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.type === "single") setJoke(data.joke);
+        else if (data.type === "twopart") setJoke(`${data.setup} ... ${data.delivery}`);
+        else setJoke("No joke found.");
+      })
+      .catch(() => setJoke("Failed to fetch joke. Try again."))
+      .finally(() => setLoading(false));
   };
 
   return (
     <div className="joke">
-      <Button callApi = {fetchApi} loading={loading}/>
+      <Button callApi={fetchApi} loading={loading} />
       <p>{joke}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Jokes
+export default Jokes;
