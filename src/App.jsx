b620d467ba8add71b7348c9d58b6a43626ea3644
Nidhi <nidhiparihar1309@gmail.com>
2025-09-29 12:47:26 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Jokes from './Components/Jokes'
 import './main.css'
 import './App.css'
+import favimg from '../src/assets/fav.png'
 
 const App = () => {
   return (
     <div>
       <div className='app'>
-        <img src="../src/assets/fav.png" alt="fav" />
+        <img src= {favimg} alt="fav" />
         <h1>Joke App</h1>
         <Jokes/>
       </div>
